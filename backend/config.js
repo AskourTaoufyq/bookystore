80908e4a028c8f409593c508573de7f8e49a334d
AskourTaoufyq <kosa477kacm@gmail.com>
2025-01-31 02:41:53 -0800

    alx

--- backend/config.js
+++
@@ -1,4 +0,0 @@
-export const PORT = 7777;
-
-export const mongoDBURL = 
-'mongodb+srv://toufiq:ta139907as@@bookystore.ahg60.mongodb.net/e-books collect?retryWrites=true&w=majority&appName=BookyStore'
